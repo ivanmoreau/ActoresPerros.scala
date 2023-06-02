1039550f52a70a37c39ea57fe02101bfc3d6cc92
Iván Molina Rebolledo <ivan@ivmoreau.com>
2023-06-02 00:11:00 -0600

    Initial compiling and running version
    Co-authored-by: Fabián Heredia Montiel <fabianhjr@protonmail.com>Co-auth

--- core/src/main/scala/com/ivmoreau/actoresperros/Actor.scala
+++ core/src/main/scala/com/ivmoreau/actoresperros/Actor.scala
@@ -1,94 +1,81 @@
 package com.ivmoreau.actoresperros
 
+import cats.effect
 import cats.implicits.*
-import cats.effect.{FiberIO, IO, Ref}
+import cats.effect.{FiberIO, IO, IOApp, Ref}
 import cats.effect.kernel.Fiber
 import cats.effect.std.Queue
 import fs2.Stream
 
-
-object Calculator extends Actor[MyEnum, MyState]:
-  def function(st: MyState): Unit = for
-    msg <- get
-  yield ()
-end Calculator
-
-// Calculator(initState) -->> ActorRef
-
 trait Actor[Msg, State]:
-  def function(st: State): IO[Unit]
-  def get: IO[Msg] = ???
-  def apply(init: State): ActorRef[Msg] = DefaultActorFactory.createActor(new ActorInternalBehaviour[Msg, State] {
-    override get
-  })
+
+  def get(using aM: ActorMethods[Msg, State]): IO[Msg] = aM.get
+
+  def function(st: State)(using ActorMethods[Msg, State]): IO[Unit]
+
+  def apply(init: State): IO[ActorRef[Msg]] = DefaultActorFactory.createActor(init).map { (ai: ActorInternal[Msg,
+    State]) =>
+    given ActorMethods[Msg, State] with
+      override def get: IO[Msg] = ai.queue.take
+    ai.actorRef(state => function(state))
+  }
 end Actor
 
-/*
-new ActorInternalBehaviour[Msg, State]:
-    override def get: IO[Msg] = queue.take
-    override def function: State => IO[Unit] = externalFunction
-*/
+trait ActorMethods[Msg, State]:
+  def get: IO[Msg]
+end ActorMethods
 
-private class ActorInternal[Msg, State]:
-  protected val queue: Queue[IO, Msg]
+private trait ActorInternal[Msg, State]:
+  protected[actoresperros] val queue: Queue[IO, Msg]
   protected val state: Ref[IO, State]
 
-  def actorRef(withBehaviour: ActorInternalBehaviour[Msg, State]): ActorRef[Msg] =
-    val fiber: IO[FiberIO[Unit]] = state.get.flatMap[Unit](withBehaviour.function).start
+  def actorRef(f: State => IO[Unit]): ActorRef[Msg] =
+    val fiber: IO[FiberIO[Unit]] = state.get.flatMap[Unit](f).start
     new ActorRef[Msg]:
       override def !(msg: Msg): IO[Unit] = queue.offer(msg)
 
       override def cancel(): IO[Unit] = fiber.flatMap(_.cancel)
 
-      override def join(): IO[Unit] = fiber.flatMap(_.join)
+      override def join(): IO[Unit] = fiber.flatMap(_.join).void
 end ActorInternal
 
 trait ActorRef[Msg]:
   def !(msg: Msg): IO[Unit]
-  def stop(): IO[Unit]
+  def cancel(): IO[Unit]
   def join(): IO[Unit]
 end ActorRef
 
-class ActorFactory:
+trait ActorFactory:
   def queueMaker[A]: IO[Queue[IO, A]]
-  def refMaker[A]: IO[Ref[IO, A]]
+  def refMaker[A]: A => IO[Ref[IO, A]]
 
-  def createActor[ActorInternal[Msg, State]](): IO[ActorInternal[Msg, State]] = for {
-    queue <- queueMaker
-    state <- initialState
-    stateRef <- refMaker
-    () <- stateRef.set(state)
-  } yield (new ActorInternal[Msg, State] {
-    override val queue: Queue[IO, Msg] = queue
+  def createActor[Msg, State](initialState: State): IO[ActorInternal[Msg, State]] = for {
+    newQueue <- queueMaker[Msg]
+    stateRef <- refMaker[State](initialState)
+  } yield new ActorInternal[Msg, State] {
+    override val queue: Queue[IO, Msg] = newQueue
     override val state: Ref[IO, State] = stateRef
-  })
+  }
 end ActorFactory
 
 object DefaultActorFactory extends ActorFactory:
-  override def queueMaker[A]: IO[Queue[IO, A]] = Queue.bounded(5)
-  override def refMaker[A]: IO[Ref[IO, A]] = Ref.of(_)
+  override def queueMaker[A]: IO[Queue[IO, A]] = Queue.bounded[IO, A](5)
+  override def refMaker[A]: A => IO[Ref[IO, A]] = Ref.of[IO, A](_)
 end DefaultActorFactory
 
+object Calculator extends Actor[Int, Unit]:
+  def function(st: Unit)(using ActorMethods[Int, Unit]): IO[Unit] = for
+    msg <- get
+    _ <- cats.effect.std.Console[IO].println(s"${msg + 420}")
+  yield ()
+end Calculator
 
-/*
-// defiterateEval[F[_], A](start: A)(f: (A) => F[A]): Stream[F, A]
-val run: IO[Unit] = msgQueue.take { a =>
-  Stream.iterateEval[IO, Msg](a)(_ => msgQueue.take).evalMap(function).compile.drain
-}.flatten
-*/
-
-// SAM
-
-class ActorInternalBehaviour[Msg, State]:
-  def get: IO[Msg]
-  def function: State => IO[Unit]
-end ActorInternalBehaviour
-
-@main def main() =
-  IO.unit *> {
+object Main extends IOApp.Simple:
+  override def run = IO.unit *> {
     for
-      _ <- DefaultActorFactory.createActor(IO.unit, _ => IO.unit)
-      // actor: ActorId <- Actor.new()
-      // _ <- actor ! "Hola"
+      calc <- Calculator(())
+      _ <- calc ! 420
+      _ <-  cats.effect.std.Console[IO].println("GO{ODPFD")
+      _ <- calc.join()
     yield ()
   }
